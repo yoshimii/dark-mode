bc3f40bf4f1fe3ebdc5e158d017d06b5dd189ea4
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-09-04 21:40:35 -0500

    added filter and to lowercase for array of searchable coin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,57 @@
 import React, { useState } from 'react';
 import useDarkMode from '../hooks/useDarkMode';
 
-const Navbar = () => {
+const Navbar = ({coinData}) => {
   const [darkMode, setDarkMode] = useDarkMode();
+  const [coinValue, setCoin] = useState('');
 
-  return (
+ 
+// let handleSearch = e => {
+//   e.preventDefault();
+//   setCoin(event.target.value);
+//   console.log(coin.value)
+// }
+
+let submitHandler = e => {
+  setCoin(coinValue);
+
+}
+ 
+
+let handleSearch = e => {
+  let currentList = [];//hold original list
+  let newList = [];//hold filtered list
+  if (e.target.value !== '') {
+    currentList = coinData;
+    console.log(currentList)
+    newList= currentList.map(coin => {
+
+        return coin.name.toLowerCase()
+      })
+      console.log(newList)
+      // const filter = e.target.value.toLowerCase();
+      // return lower.includes(filter);
+  }else {
+    newList = coinData;
+  }
+  setCoin(newList);
+}
+
+  return ([
+
+
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
+      <form onSubmit={submitHandler}
+      className='search-container'>
+    <input 
+    type='text'
+    value={coinValue}
+    id='search-bar'
+    placeholder='Find your coin'
+    onChange={handleSearch}
+    ></input>
+  </form>
       <div className="dark-mode__toggle">
         <div
           onClick={setDarkMode}
@@ -14,7 +59,7 @@ const Navbar = () => {
         />
       </div>
     </nav>
-  );
+  ]);
 };
 
 export default Navbar;
--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const App = () => {
   }, []);
   return (
     <div className="App">
-      <Navbar />
+      <Navbar coinData={coinData}/>
       <Charts coinData={coinData} />
     </div>
   );
