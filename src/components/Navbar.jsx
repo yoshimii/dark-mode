e49b0d02787285ba8e0a02544b161020cdecfc18
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-09-04 23:55:51 -0500

    made useSearch file and handleSearch returns coin name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,65 +1,54 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import useDarkMode from '../hooks/useDarkMode';
 
 const Navbar = ({coinData}) => {
-  const [darkMode, setDarkMode] = useDarkMode();
-  const [coinValue, setCoin] = useState('');
+    const [darkMode, setDarkMode] = useDarkMode();
+    const [coinValue, setCoin] = useState('');
+    const [search, setSearch] = useState('');
 
- 
-// let handleSearch = e => {
-//   e.preventDefault();
-//   setCoin(event.target.value);
-//   console.log(coin.value)
-// }
+    let updateSearch = e => {
+      setSearch(e.target.value)
+    }
+    let handleSearch = e => {
+        e.preventDefault();
+        let currentList = []; // hold original list
+        let newList = []; // hold filtered list
+        if (e.target.value !== '') {
+            currentList = coinData;
+            console.log(currentList)
+            newList = currentList.map(coin => {
 
-let submitHandler = e => {
-  setCoin(coinValue);
+                return coin.name.toLowerCase()
+            })
 
-}
- 
+        } else {
+            newList = coinData;
+        }
+        setCoin(newList);
+        console.log(coinValue)
+    }
 
-let handleSearch = e => {
-  let currentList = [];//hold original list
-  let newList = [];//hold filtered list
-  if (e.target.value !== '') {
-    currentList = coinData;
-    console.log(currentList)
-    newList= currentList.map(coin => {
-
-        return coin.name.toLowerCase()
-      })
-      console.log(newList)
-      // const filter = e.target.value.toLowerCase();
-      // return lower.includes(filter);
-  }else {
-    newList = coinData;
-  }
-  setCoin(newList);
-}
-
-  return ([
+    return([
 
 
-    <nav className="navbar">
-      <h1>Crypto Tracker</h1>
-      <form onSubmit={submitHandler}
-      className='search-container'>
-    <input 
-    type='text'
-    value={coinValue}
-    id='search-bar'
-    placeholder='Find your coin'
-    onChange={handleSearch}
-    ></input>
-  </form>
-      <div className="dark-mode__toggle">
-        <div
-          onClick={setDarkMode}
-          className={darkMode ? 'toggle toggled' : 'toggle'}
-        />
-      </div>
-    </nav>
-  ]);
+        <nav className="navbar">
+            <h1>Crypto Tracker</h1>
+            <form onSubmit={handleSearch}
+                className='search-container'>
+                <input type='text'
+                    value={search}
+                    id='search-bar'
+                    placeholder='Find your coin'
+                    onChange={updateSearch}></input>
+            </form>
+            <div className="dark-mode__toggle">
+                <div onClick={setDarkMode}
+                    className={
+                        darkMode ? 'toggle toggled' : 'toggle'
+                    }/>
+            </div>
+        </nav>
+    ]);
 };
 
 export default Navbar;
